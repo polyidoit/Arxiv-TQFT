d0de4d618b0d036f5c2424daee2b3232d585b91b
JiaGithub <giannjia@foxmail.com>
2023-03-27 00:00:39 +0800

    Update daily_arxiv.py

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -283,8 +283,8 @@ def json_to_md(filename,md_filename,
         else:
             f.write("> Updated on " + DateNow + "\n")
 
-        # TODO: add usage
-        #f.write("> Usage instructions: [here](./docs/README.md#usage)\n\n")
+        TODO: add usage
+        f.write("> Usage instructions: [here](./README.md)\n\n")
 
         #Add: table of contents
         if use_tc == True:
@@ -329,6 +329,10 @@ def json_to_md(filename,md_filename,
                 top_info = top_info.replace(' ','-').replace('.','')
                 f.write(f"<p align=right>(<a href={top_info}>back to top</a>)</p>\n\n")
             
+            if show_badge == False:
+            # we don't like long string, break it!
+            f.write((f"[contributors-shield]: https://img.shields.io/github/"
+                     f"contributors/Vincentqyw/cv-arxiv-daily.svg?style=for-the-badge\n"))
      
                 
     logging.info(f"{task} finished")        
