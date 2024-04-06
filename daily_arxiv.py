e1f0453c3280c230ae8373890af8dab2b45c7777
JiaGithub <giannjia@foxmail.com>
2024-04-07 00:17:42 +0800

    Update daily_arxiv.py

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -47,17 +47,13 @@ def load_config(config_file:str) -> dict:
         logging.info(f'config = {config}')
     return config 
 
-def get_authors(authors):
-    """
-    Concatenate all authors' names with a comma.
-    
-    Parameters:
-        authors (list): List of author names.
-        
-    Returns:
-        str: String containing all authors' names separated by a comma.
-    """
-    return ", ".join(authors)
+def get_authors(authors, first_author = False):
+    output = str()
+    if first_author == False:
+        output = ", ".join(str(author) for author in authors)
+    else:
+        output = authors[0]
+    return output
 
 def sort_papers(papers):
     output = dict()
@@ -141,16 +137,16 @@ def get_daily_papers(topic,query="slam", max_results=2):
             #    if repo_url is None:
             #        repo_url = get_code_link(paper_key)
             if repo_url is not None:
-                content[paper_key] = "|**{}**|**{}**|{}|{} et.al.|[{}]({})|**[link]({})**|\n".format(
-                       update_time,paper_title,paper_authors,paper_first_author,paper_key,paper_url,repo_url)
+                content[paper_key] = "|**{}**|**{}**|{} et.al.|[{}]({})|**[link]({})**|\n".format(
+                       update_time,paper_title,paper_first_author,paper_key,paper_url,repo_url)
                 content_to_web[paper_key] = "- {}, **{}**, {} et.al., Paper: [{}]({}), Code: **[{}]({})**".format(
-                       update_time,paper_title,paper_authors,paper_first_author,paper_url,paper_url,repo_url,repo_url)
+                       update_time,paper_title,paper_first_author,paper_url,paper_url,repo_url,repo_url)
 
             else:
-                content[paper_key] = "|**{}**|**{}**|{}|{} et.al.|[{}]({})|null|\n".format(
-                       update_time,paper_title,paper_authors,paper_first_author,paper_key,paper_url)
+                content[paper_key] = "|**{}**|**{}**|{} et.al.|[{}]({})|null|\n".format(
+                       update_time,paper_title,paper_first_author,paper_key,paper_url)
                 content_to_web[paper_key] = "- {}, **{}**, {} et.al., Paper: [{}]({})".format(
-                       update_time,paper_title,paper_authors,paper_first_author,paper_url,paper_url)
+                       update_time,paper_title,paper_first_author,paper_url,paper_url)
 
             # TODO: select useful comments
             comments = None
