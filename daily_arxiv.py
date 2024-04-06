ad9bdc6d2f1dde85c672d3bf6e21a154ec193ffb
JiaGithub <giannjia@foxmail.com>
2024-04-06 22:37:24 +0800

    change of authors

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -135,16 +135,16 @@ def get_daily_papers(topic,query="slam", max_results=2):
             #    if repo_url is None:
             #        repo_url = get_code_link(paper_key)
             if repo_url is not None:
-                content[paper_key] = "|**{}**|**{}**|{} et.al.|[{}]({})|**[link]({})**|\n".format(
-                       update_time,paper_title,paper_first_author,paper_key,paper_url,repo_url)
+                content[paper_key] = "|**{}**|**{}**|{}|[{}]({})|**[link]({})**|\n".format(
+                       update_time,paper_title,paper_authors,paper_key,paper_url,repo_url)
                 content_to_web[paper_key] = "- {}, **{}**, {} et.al., Paper: [{}]({}), Code: **[{}]({})**".format(
-                       update_time,paper_title,paper_first_author,paper_url,paper_url,repo_url,repo_url)
+                       update_time,paper_title,paper_authors,paper_url,paper_url,repo_url,repo_url)
 
             else:
-                content[paper_key] = "|**{}**|**{}**|{} et.al.|[{}]({})|null|\n".format(
-                       update_time,paper_title,paper_first_author,paper_key,paper_url)
+                content[paper_key] = "|**{}**|**{}**|{}|[{}]({})|null|\n".format(
+                       update_time,paper_title,paper_authors,paper_key,paper_url)
                 content_to_web[paper_key] = "- {}, **{}**, {} et.al., Paper: [{}]({})".format(
-                       update_time,paper_title,paper_first_author,paper_url,paper_url)
+                       update_time,paper_title,paper_authors,paper_url,paper_url)
 
             # TODO: select useful comments
             comments = None
